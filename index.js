4ac25aa3f8b8d449544d457f41ca6280a321cf0d
zahidhasan2806 <jahidh714@gmail.comm>
2021-11-10 03:01:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleaware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xohwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully');
+
+    } finally {
+        //await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send(' server is running');
+});
+
+app.listen(port, () => {
+    console.log('server running at port', port);
+});
