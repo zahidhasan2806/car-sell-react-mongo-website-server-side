a46029e19716ff78fac3ccf12edbeba76cdb27d0
zahidhasan2806 <jahidh714@gmail.comm>
2021-11-23 20:35:27 +0600

    image upload functionality

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const fileUpload = require('express-fileupload')
+
+
 //middleaware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
+
+const stripe = require("stripe")('sk_test_51JwViFDfWpg2oNRP0qz90M93juHSN8aingBoJlEKpPVmD2DpAAiWXNPenBOELWm7x7Vz6435EWIvEWMOzfpekYSt00UMNBfBPS');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xohwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -40,9 +46,19 @@ async function run() {
 
         //POST API for  new products
         app.post("/cars", async (req, res) => {
-            const newPackage = (req.body);
-            const result = await carsCollection.insertOne(newPackage);
-            console.log(result);
+            const name = req.body.name;
+            const description = req.body.description;
+            const price = req.body.price;
+            const Category = req.body.Category;
+            const pictures = req.files.image;
+            const pictureData = pictures.data;
+            const encodedData = pictureData.toString('base64')
+            const imgBuffer = Buffer.from(encodedData, 'base64')
+            const data = {
+                name, description, price, Category,
+                image: imgBuffer
+            }
+            const result = await carsCollection.insertOne(data);
             res.json(result);
         })
 
@@ -63,6 +79,12 @@ async function run() {
             const orders = await ordersCollection.find({}).toArray();
             res.send(orders);
         });
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const orders = await ordersCollection.findOne(query);
+            res.send(orders);
+        });
 
         //Delete API- delete order
         app.delete('/orders/:id', async (req, res) => {
@@ -75,7 +97,10 @@ async function run() {
             const order = req.body;
             const options = { upsert: true };
             const updatedOrder = {
-                $set: { status: order.status }
+                $set: {
+                    status: order.status,
+                    payment: order
+                }
             };
 
             const updateStatus = await ordersCollection.updateOne({ _id: ObjectId(req.params.id) }, updatedOrder, options,)
@@ -131,7 +156,24 @@ async function run() {
             res.send(reviews);
         });
 
-        console.log('database connected successfully');
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const paymentInfo = req.body;
+            console.log(paymentInfo)
+            const amount = paymentInfo.carPrice * 100;
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: "usd",
+                amount: amount,
+                payment_method_types: [
+                    "card",
+                ],
+            });
+
+            res.json({
+                clientSecret: paymentIntent.client_secret
+            });
+        });
 
     } finally {
         //await client.close();
